c450c7432300b80225aa18c1bc1a057223025698
The_Hufaaz <ufasabdulmutolib@gmail.com>
2023-05-14 23:05:05 +0100

    Remove date base url

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://abdul:KeZlHiFbVA5gLS5e@cluster0.ummun8r.mongodb.net/test");
+    const conn = await mongoose.connect("");
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
